01c5acea38378ee51104ae0510d72428d0b8b6c8
mariusvh <mariusvh@stud.ntnu.no>
2019-03-23 13:10:05 +0100

    Make main

--- main.go
+++ main.go
@@ -1,9 +1,33 @@
 package main
 
 import (
-	"fmt"
+	"github.com/sigtot/elevio"
+	"github.com/sigtot/sanntid/buttons"
+	"github.com/sigtot/sanntid/buyer"
+	"github.com/sigtot/sanntid/elev"
+	"github.com/sigtot/sanntid/indicators"
+	"github.com/sigtot/sanntid/orders"
+	"github.com/sigtot/sanntid/seller"
+	"github.com/sigtot/sanntid/types"
 )
 
 func main() {
-	fmt.Println("It is wednesday my dudes")
+	goalArrivals := make(chan types.Order)
+	currentGoals := make(chan types.Order)
+	floorArrivals := make(chan int)
+	go elevio.PollFloorSensor(floorArrivals)
+	elevator := elev.StartElevController(goalArrivals, currentGoals, floorArrivals)
+
+	callsForSale := make(chan types.Call)
+	buttonEvents := make(chan elevio.ButtonEvent)
+	go elevio.PollButtons(buttonEvents)
+	buttons.StartButtonHandler(buttonEvents, callsForSale)
+
+	indicators.StartIndicatorHandler()
+
+	oh, newOrders := orders.StartOrderHandler(currentGoals, goalArrivals, elevator)
+
+	buyer.StartBuying(oh, newOrders)
+
+	seller.StartSelling(callsForSale) //Go this
 }
