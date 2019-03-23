3f86b9c6596bd68a47b7d64f7135da93bb13c8d6
mariusvh <mariusvh@stud.ntnu.no>
2019-03-23 13:09:11 +0100

    Extend elev test

--- elev/elev_test.go
+++ elev/elev_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/sigtot/sanntid/orders"
 	"github.com/sigtot/sanntid/types"
 	"testing"
+	"time"
 )
 
 func TestInit(t *testing.T) {
@@ -29,13 +30,42 @@ func TestPollFloor(t *testing.T) {
 func TestStartElevController(t *testing.T) {
 	goalArrivals := make(chan types.Order)
 	currentGoals := make(chan types.Order)
+	floorArrivals := make(chan int)
+	go elevio.PollFloorSensor(floorArrivals)
 
-	_ = StartElevController(goalArrivals, currentGoals)
+	_ = StartElevController(goalArrivals, currentGoals, floorArrivals)
 
-	newOrder := types.Order{Call: types.Call{Type: types.Hall, Floor: 2, Dir: types.Up}}
-	currentGoals <- newOrder
+	firstOrder := types.Order{Call: types.Call{Type: types.Hall, Floor: 3, Dir: types.Down}}
+	secondOrder := types.Order{Call: types.Call{Type: types.Cab, Floor: 2, Dir: types.InvalidDir}}
+	currentGoals <- firstOrder
 	arrived := <-goalArrivals
-	if !orders.OrdersEqual(newOrder, arrived) {
+	if !orders.OrdersEqual(firstOrder, arrived) {
+		t.Fatal("Orders not equal")
+	}
+	currentGoals <- secondOrder
+	arrived = <-goalArrivals
+	if !orders.OrdersEqual(secondOrder, arrived) {
+		t.Fatal("Orders not equal")
+	}
+}
+
+func TestGoalOverride(t *testing.T) {
+	goalArrivals := make(chan types.Order)
+	currentGoals := make(chan types.Order)
+	floorArrivals := make(chan int)
+	go elevio.PollFloorSensor(floorArrivals)
+
+	_ = StartElevController(goalArrivals, currentGoals, floorArrivals)
+
+	firstOrder := types.Order{Call: types.Call{Type: types.Hall, Floor: 3, Dir: types.Down}}
+	secondOrder := types.Order{Call: types.Call{Type: types.Cab, Floor: 0, Dir: types.InvalidDir}}
+
+	currentGoals <- firstOrder
+	time.Sleep(2000 * time.Millisecond)
+	currentGoals <- secondOrder
+
+	arrived := <-goalArrivals
+	if !orders.OrdersEqual(secondOrder, arrived) {
 		t.Fatal("Orders not equal")
 	}
 }
