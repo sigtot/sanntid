26ce9c0cb2353fc6f2e8d3364f0402d42acfa443
sigtot <sigurdtotland@gmail.com>
2019-03-23 23:48:47 +0100

    Implement the parts of order watcher responsible persisting acked orders

---
+++ orderwatcher/order_watcher.go
@@ -0,0 +1,151 @@
+package orderwatcher
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/Sirupsen/logrus"
+	bolt "github.com/etcd-io/bbolt"
+	"github.com/sigtot/sanntid/pubsub"
+	"github.com/sigtot/sanntid/pubsub/subscribe"
+	"github.com/sigtot/sanntid/types"
+	"math/rand"
+	"strconv"
+	"time"
+)
+
+const hallUpBucketName = "hall_up"
+const hallDownBucketName = "hall_down"
+
+const dbTraversalInterval = 500
+
+const baseTTD = 10000
+const randTTDPart = 2000
+
+type WatchThis struct {
+	ElevatorID string
+	Time       time.Time
+	Call       types.Call
+}
+
+func StartOrderWatcher(callsForSale chan types.Call, db *bolt.DB) {
+	okOrPanic(initHallOrderBuckets(db))
+
+	ackSubChan, _ := subscribe.StartSubscriber(pubsub.AckDiscoveryPort)
+	orderDeliveredSubChan, _ := subscribe.StartSubscriber(pubsub.OrderDeliveredDiscoveryPort)
+
+	log := logrus.New()
+	go func() {
+		dbTraversalTicker := time.NewTicker(dbTraversalInterval * time.Millisecond)
+		for {
+			select {
+			case ackJson := <-ackSubChan:
+				ack := types.Ack{}
+				err := json.Unmarshal(ackJson, &ack)
+				if err != nil {
+					panic(fmt.Sprintf("Could not unmarshal ack %s", err.Error()))
+				}
+				wt := WatchThis{ElevatorID: ack.ElevatorID, Time: time.Now(), Call: ack.Call}
+				wtJson, err := json.Marshal(wt)
+				if err != nil {
+					panic("Could not marshal WatchThis")
+				}
+				if wt.Call.Type == types.Hall {
+					var bName string
+					if wt.Call.Dir == types.Up {
+						bName = hallUpBucketName
+					} else if wt.Call.Dir == types.Down {
+						bName = hallDownBucketName
+					} else {
+						panic("unexpected non-direction")
+					}
+					err = db.Update(func(tx *bolt.Tx) error {
+						b := tx.Bucket([]byte(bName))
+
+						err = b.Put([]byte(strconv.Itoa(wt.Call.Floor)), []byte(wtJson))
+						return err
+					})
+				} else {
+					err = db.Update(func(tx *bolt.Tx) error {
+						if b, err := tx.CreateBucketIfNotExists([]byte(wt.ElevatorID)); err == nil {
+							err = b.Put([]byte(strconv.Itoa(wt.Call.Floor)), []byte(wtJson))
+						}
+						return err
+					})
+					okOrPanic(err)
+				}
+			case orderJson := <-orderDeliveredSubChan:
+				// TODO: Implement
+				order := types.Order{}
+				err := json.Unmarshal(orderJson, &order)
+				if err != nil {
+					panic(fmt.Sprintf("Could not unmarshal order %s", err.Error()))
+				}
+			case <-dbTraversalTicker.C:
+				err := db.View(func(tx *bolt.Tx) error {
+					return tx.ForEach(func(name []byte, b *bolt.Bucket) error {
+						err := b.ForEach(func(k []byte, v []byte) error {
+							if wt, err := unmarshalWatchThis(v); err == nil {
+								if time.Now().After(wt.Time.Add(getTTD())) {
+									// Resell order
+									callsForSale <- wt.Call
+
+									// Update time
+									wt.Time = time.Now()
+									if wtJson, err := json.Marshal(wt); err == nil {
+										return b.Put(k, wtJson)
+									}
+									logWatchThis(log, "ORDER WATCHER", "Sent order to seller for resale", *wt)
+									return err
+								}
+							} else {
+								return err
+							}
+							return nil
+						})
+						return err
+					})
+				})
+				okOrPanic(err)
+			}
+		}
+	}()
+}
+
+func unmarshalWatchThis(wtJson []byte) (*WatchThis, error) {
+	wt := WatchThis{}
+	err := json.Unmarshal(wtJson, &wt)
+	return &wt, err
+}
+
+func initHallOrderBuckets(db *bolt.DB) error {
+	return db.Update(func(tx *bolt.Tx) error {
+		if _, err := tx.CreateBucketIfNotExists([]byte(hallUpBucketName)); err != nil {
+			return err
+		}
+		if _, err := tx.CreateBucketIfNotExists([]byte(hallDownBucketName)); err != nil {
+			return err
+		}
+		return nil
+	})
+}
+
+func okOrPanic(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+// Returns time to delivery for order, randomly distributed around its base time
+func getTTD() time.Duration {
+	return time.Duration(baseTTD+(rand.Intn(randTTDPart)-randTTDPart/2)) * time.Millisecond
+}
+
+func logWatchThis(log *logrus.Logger, moduleName string, info string, wt WatchThis) {
+	log.WithFields(logrus.Fields{
+		"type":  wt.Call.Type,
+		"floor": wt.Call.Floor,
+		"dir":   wt.Call.Dir,
+		"id":    wt.ElevatorID,
+		"time":  wt.Time,
+	}).Infof("%-15s %s", moduleName, info)
+}
---
+++ orderwatcher/order_watcher_test.go
@@ -0,0 +1,104 @@
+package orderwatcher
+
+import (
+	"encoding/json"
+	bolt "github.com/etcd-io/bbolt"
+	"github.com/sigtot/sanntid/pubsub"
+	"github.com/sigtot/sanntid/pubsub/publish"
+	"github.com/sigtot/sanntid/types"
+	"os"
+	"strconv"
+	"testing"
+	"time"
+)
+
+const testDbName = "test.db"
+const testElevID = "cb:32:f6:7e:2d:cc"
+const testDbPerms = 0600
+const testDbTimeout = 1000
+
+func TestInitHallOrderBuckets(t *testing.T) {
+	db, err := bolt.Open(testDbName, testDbPerms, &bolt.Options{Timeout: testDbTimeout * time.Millisecond})
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		if err := db.Close(); err != nil {
+			panic(err)
+		}
+	}()
+	err = initHallOrderBuckets(db)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = db.View(func(tx *bolt.Tx) error {
+		upB := tx.Bucket([]byte(hallUpBucketName))
+		downB := tx.Bucket([]byte(hallDownBucketName))
+		if upB == nil || downB == nil {
+			t.Fatal("Expected buckets to be created successfully, but they are nil after creation")
+		}
+		return nil
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := os.Remove(testDbName); err != nil {
+		panic(err)
+	}
+}
+
+func TestStartOrderWatcher(t *testing.T) {
+	db, err := bolt.Open(testDbName, testDbPerms, &bolt.Options{Timeout: testDbTimeout * time.Millisecond})
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		if err := db.Close(); err != nil {
+			panic(err)
+		}
+	}()
+	ackPubChan := publish.StartPublisher(pubsub.AckDiscoveryPort)
+
+	callsForSale := make(chan types.Call)
+	StartOrderWatcher(callsForSale, db)
+	orders := []types.Order{
+		{Call: types.Call{Type: types.Hall, Dir: types.Up, Floor: 1}},
+		{Call: types.Call{Type: types.Hall, Dir: types.Down, Floor: 6}},
+		{Call: types.Call{Type: types.Cab, Dir: types.InvalidDir, Floor: 0}},
+	}
+
+	for _, v := range orders {
+		ackJson, err := json.Marshal(types.Ack{Bid: types.Bid{Call: v.Call, Price: 2, ElevatorID: testElevID}})
+		if err != nil {
+			t.Fatal("Could not marshal ack")
+		}
+		ackPubChan <- ackJson
+	}
+
+	time.Sleep(1000 * time.Millisecond)
+
+	err = db.View(func(tx *bolt.Tx) error {
+		buckets := []*bolt.Bucket{
+			tx.Bucket([]byte(hallUpBucketName)),
+			tx.Bucket([]byte(hallDownBucketName)),
+			tx.Bucket([]byte(testElevID)),
+		}
+		for i, b := range buckets {
+			var retrievedWT WatchThis
+			if err := json.Unmarshal(b.Get([]byte(strconv.Itoa(orders[i].Floor))), &retrievedWT); err != nil {
+				t.Fatal(err)
+			}
+
+			if !(retrievedWT.Call == orders[i].Call) {
+				t.Fatalf("Retrieved value %+v does not match %+v\n", retrievedWT, orders[i].Call)
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := os.Remove(testDbName); err != nil {
+		panic(err)
+	}
+}
