58dbce2eca65c5ef26c7527a62794905035f93cd
sigtot <sigurdtotland@gmail.com>
2019-04-01 17:31:16 +0200

    Remove echo in entrypoint.sh

--- display/entrypoint.sh
+++ display/entrypoint.sh
@@ -7,7 +7,7 @@ num=`nmap -sS -p 22 --open $subnetprefix.0/24 -oG - | grep 22 | grep Host | grep
 echo "Discovered $num elevators, connecting..."
 num=$(( $num + 0 ))
 iter=$num
-for ip in `nmap -sS -p 22 --open $subnetprefix.0/24 -oG - | grep 22 | grep Host | grep -oP "\d+\.\d+\.\d+\.\d+"`; do if [ $iter -eq $num ]; then tmux new-session -d -s elevs "sshpass -p pass ssh -t -o \"StrictHostKeyChecking=no\" elev@$ip 'tmux a'"; else tmux splitw -p $((100/$num)) -d -t elevs "sshpass -p pass ssh -t -o \"StrictHostKeyChecking=no\" elev@$ip 'tmux a'"; fi; iter=$(($iter - 1)); echo $iter; done
+for ip in `nmap -sS -p 22 --open $subnetprefix.0/24 -oG - | grep 22 | grep Host | grep -oP "\d+\.\d+\.\d+\.\d+"`; do if [ $iter -eq $num ]; then tmux new-session -d -s elevs "sshpass -p pass ssh -t -o \"StrictHostKeyChecking=no\" elev@$ip 'tmux a'"; else tmux splitw -p $((100/$num)) -d -t elevs "sshpass -p pass ssh -t -o \"StrictHostKeyChecking=no\" elev@$ip 'tmux a'"; fi; iter=$(($iter - 1)); done
 tmux select-layout -t elevs even-vertical
 echo Display server started. Type 'docker exec -ti display bash -c "tmux a -t elevs"' to enter
 tail -f /dev/null
