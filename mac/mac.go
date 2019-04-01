8186aff31702c8e26747e31717f500c9c0d74de2
sigtot <sigurdtotland@gmail.com>
2019-04-01 16:14:23 +0200

    Restructure GetMacAddr

--- mac/mac.go
+++ mac/mac.go
@@ -4,23 +4,30 @@ Package mac contains the functionality for finding the eno1, eth1 or eth0 mac-ad
 package mac
 
 import (
-	"fmt"
 	"net"
 )
 
+var interfaceNames = []string{"eno1", "eth1", "eth0"}
+
 // GetMacAddr gets the network-cards mac address
 func GetMacAddr() (string, error) {
-	mac, err := net.InterfaceByName("eno1")
-	if err != nil {
-		mac, err = net.InterfaceByName("eth1")
-		if err != nil {
-			mac, err = net.InterfaceByName("eth0")
-			if err != nil {
-				panic(fmt.Sprintf("No MAC address found. %s", err.Error()))
-			}
+	err := error(nil)
+	for _, interfaceName := range interfaceNames {
+		mac, newErr := net.InterfaceByName(interfaceName)
+		err = mergeErrors(err, newErr)
+		if err == nil {
+			return mac.HardwareAddr.String(), err
 		}
 	}
-	as := mac.HardwareAddr.String()
+	return "", err
+}
 
-	return as, nil
+func mergeErrors(oldErr error, newErr error) error {
+	if oldErr != nil {
+		return oldErr
+	}
+	if newErr != nil {
+		return newErr
+	}
+	return nil
 }
